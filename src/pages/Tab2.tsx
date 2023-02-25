46f146d8d86c9dc19d4ff0abf00efe2a371202c9
Steven Rivera <shrivera@uci.edu>
2023-02-24 22:41:49 -0800

    Connected to backend and got dynamic restaurant card

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -6,24 +6,50 @@ import {
   IonToolbar,
   IonRow,
   IonCol,
-  IonItem,
-  IonLabel,
-  IonInput,
-  IonGrid,
-  IonSelect,
-  IonSelectOption,
-  IonCheckbox,
+  IonSearchbar,
   IonButton,
-  IonIcon,
-  IonCard,
-  IonCardHeader,
-  IonCardTitle,
-  IonCardSubtitle,
-  IonCardContent,
+  IonGrid,
 } from '@ionic/react';
+import { useRef, useState } from 'react';
 import './css/Tab2.css';
+import RestaurantCard from '../components/RestaurantCard';
 
 const Tab2: React.FC = () => {
+  const searchRef = useRef<HTMLIonSearchbarElement>(null);
+  const [restaurants, setRestaurants] = useState<JSX.Element[]>([]);
+
+  const getSearch = async () => {
+    // const searchKey = searchRef.current?.value;
+    // console.log(searchKey);
+    const url = "http://127.0.0.1:5000/v1/user/1234/nearby";
+    const response = await fetch(url);
+    const nearby = await response.json();
+
+    var elements = [];
+    for (var i = 0; i < nearby.businesses.length; i += 2) {
+      var first = nearby.businesses[i];
+      var second = (i + 1 < nearby.businesses.length) ? nearby.businesses[i + 1] : null
+
+      elements.push((
+        <IonGrid fixed={true} key={i}>
+          <IonRow>
+            <IonCol>
+              <RestaurantCard img_url={first.image_url} name={first.name} rating={first.rating} address={first.address} />
+            </IonCol>
+            <IonCol>
+              {second ?
+                <RestaurantCard img_url={second.image_url} name={second.name} rating={second.rating} address={second.address} />
+                : null}
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      ))
+    };
+
+    setRestaurants(elements);
+  };
+
+
   return (
     <IonPage>
 
@@ -36,88 +62,18 @@ const Tab2: React.FC = () => {
 
       <IonContent fullscreen>
 
-        <IonRow>
+        <IonGrid>
+          <IonRow>
+            <IonCol size="12">
+              <IonSearchbar ref={searchRef} showClearButton="focus" inputmode="text"></IonSearchbar>
+            </IonCol>
+            <IonCol class="ion-text-center">
+              <IonButton onClick={getSearch} shape="round" fill="solid" size="small">Enter</IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
 
-          <IonCol>
-            <IonCard>
-              <img alt="Sessions West Coast Deli" src="https://s3-media2.fl.yelpcdn.com/bphoto/q72MWqqmQuMTL-WbuDo1sQ/o.jpg" />
-              <IonCardHeader>
-                <IonCardTitle>Sessions West Coast Deli</IonCardTitle>
-                <IonCardSubtitle>Rating: 4.0</IonCardSubtitle>
-              </IonCardHeader>
-
-              <IonCardContent>4736 Barranca Pkwy, Irvine, CA 92604</IonCardContent>
-            </IonCard>
-          </IonCol>
-
-          <IonCol>
-            <IonCard>
-              <img alt="S'wich Bistro" src="https://s3-media2.fl.yelpcdn.com/bphoto/wFtJFW-ghuRGF8GOjBZoSA/o.jpg" />
-              <IonCardHeader>
-                <IonCardTitle>S'wich Bistro</IonCardTitle>
-                <IonCardSubtitle>Rating: 4.5</IonCardSubtitle>
-              </IonCardHeader>
-
-              <IonCardContent>16277 Laguna Canyon Rd, Unit B, Irvine, CA 92618</IonCardContent>
-            </IonCard>
-          </IonCol>
-
-          <IonCol>
-            <IonCard>
-              <img alt="Kosher Bite" src="https://s3-media3.fl.yelpcdn.com/bphoto/klkHX14eAFW4RhfzTS5IVg/o.jpg" />
-              <IonCardHeader>
-                <IonCardTitle>Kosher Bite</IonCardTitle>
-                <IonCardSubtitle>Rating: 4.5</IonCardSubtitle>
-              </IonCardHeader>
-
-              <IonCardContent>23595 Moulton Pkwy, Ste H , Laguna Hills, CA 92653</IonCardContent>
-            </IonCard>
-          </IonCol>
-
-        </IonRow>
-
-        <IonRow>
-          <IonCol>
-            <IonCard>
-              <img alt="Moe's Deli & Catering" src="https://s3-media1.fl.yelpcdn.com/bphoto/9280aAhZ28FmVaU-9k3aDA/o.jpg" />
-              <IonCardHeader>
-                <IonCardTitle>Moe's Deli & Catering</IonCardTitle>
-                <IonCardSubtitle>Rating: 4.5</IonCardSubtitle>
-              </IonCardHeader>
-
-              <IonCardContent>23052 Lake Forest Dr, Laguna Hills, CA 92653</IonCardContent>
-            </IonCard>
-          </IonCol>
-
-          <IonCol>
-            <IonCard>
-              <img alt="Larry's Catering & Deli" src="https://s3-media4.fl.yelpcdn.com/bphoto/vsZ1GOscPzqGuPlWduQKRQ/o.jpg" />
-              <IonCardHeader>
-                <IonCardTitle>Larry's Catering & Deli</IonCardTitle>
-                <IonCardSubtitle>Rating: 4.5</IonCardSubtitle>
-              </IonCardHeader>
-
-              <IonCardContent>17145 Von Karman Ave, Ste 102, Irvine, CA 92614</IonCardContent>
-            </IonCard>
-          </IonCol>
-
-          <IonCol>
-            <IonCard>
-              <img alt="Mr Sandwich" src="https://s3-media1.fl.yelpcdn.com/bphoto/wr-QweTgXz-jgOFtICqDOg/o.jpg" />
-              <IonCardHeader>
-                <IonCardTitle>Mr Sandwich</IonCardTitle>
-                <IonCardSubtitle>Rating: 4.5</IonCardSubtitle>
-              </IonCardHeader>
-
-              <IonCardContent>92 Corporate Park ,Ste J ,Irvine, CA 92606</IonCardContent>
-            </IonCard>
-          </IonCol>
-        </IonRow>
-
-
-        <IonItem lines="full">
-          <IonLabel>Food Prefrences</IonLabel>
-        </IonItem>
+        {restaurants}
 
       </IonContent>
 
