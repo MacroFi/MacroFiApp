9e70fe8c0b2be9c4019a42ef03a9ed9f4bfe5e45
Shoaib Bohra <67881729+EndUserAgreement@users.noreply.github.com>
2023-03-08 04:03:02 -0800

    Nutritional Data beginning
    Working on Nutritional Data as well as set up for other components

---
+++ src/components/Location.tsx
@@ -0,0 +1,33 @@
+import {
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonItem,
+    IonLabel,
+    IonRow,
+    IonCol,
+    IonContent,
+    IonGrid
+  } from '@ionic/react';
+
+//import { Geolocation, Geoposition } from '@ionic-native/geolocation';
+
+
+const Location: React.FC = () => {
+
+    return (
+      <IonPage>
+  
+
+  
+  
+        <IonContent>
+          
+        </IonContent>
+  
+      </IonPage>
+    );
+  };
+    
+  export default Location;
--- src/pages/PersonalDataTab/NutritionalDataPage.tsx
+++ src/pages/PersonalDataTab/NutritionalDataPage.tsx
@@ -1,3 +1,4 @@
+import React, { useRef, useState } from "react";
 import {
   IonHeader,
   IonContent,
@@ -7,13 +8,51 @@ import {
   IonBackButton,
   IonItem,
   IonLabel,
-  IonPage
+  IonPage,
+  IonGrid,
+  IonRow,
+  IonCol
 } from '@ionic/react';
 
 import '../css/Tab1.css';
+//import NutrientDataDisplay from '../../components/NutrientDataDisplay';
 
 
 const NutritionalDataPage: React.FC = () => {
+
+  interface NutritionalData {
+    calories: string | number | null | undefined;
+    fat: string | number | null | undefined;
+    protein: string | number | null | undefined;
+    cholesterol: string | number | null | undefined;
+    vitamina: string | number | null | undefined;
+    vitaminc: string | number | null | undefined;
+    calcium: string | number | null | undefined;
+    iron: string | number | null | undefined;
+    sodium: string | number | null | undefined;
+  }
+  
+  const getLocalData = () => {
+    const local = window.localStorage.getItem("data")
+    if (local == null) {
+      return null
+    }
+    return JSON.parse(local)
+  }
+  
+  //https://www.livestrong.com/article/440416-fda-daily-nutritional-requirements/
+  const [NutritionalData] = useState({
+    calories: 2000,
+    fat: 65,    //grams
+    protein: 50,
+    cholesterol: 0,
+    vitamina: 0,
+    vitaminc: 0,
+    calcium: 0,
+    iron: 0,
+    sodium: 0,
+  });
+
 
   return (
     <IonPage>
@@ -29,6 +68,17 @@ const NutritionalDataPage: React.FC = () => {
       </IonHeader>
 
       <IonContent>
+
+
+          <IonHeader> <IonItem>Current Reccomended Daily Macronutrient Intake</IonItem> </IonHeader>
+          <IonItem>Daily Caloric Reccomendation: </IonItem>
+          
+          <IonItem>Daily Calcium Reccomendation: </IonItem>
+
+
+          <IonItem>Daily Caloric Reccomendation: </IonItem>
+          <IonItem>Daily Caloric Reccomendation: </IonItem>
+
       </IonContent>
 
     </IonPage>
