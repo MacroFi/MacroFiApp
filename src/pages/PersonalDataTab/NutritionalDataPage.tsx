bd894ff8d16e0b77b5c34f2d21ecec5127fee46f
Shoaib Bohra <67881729+EndUserAgreement@users.noreply.github.com>
2023-03-12 17:33:03 -0700

    Small changes to nutritionaldatapage, more to come to

--- src/pages/PersonalDataTab/NutritionalDataPage.tsx
+++ src/pages/PersonalDataTab/NutritionalDataPage.tsx
@@ -14,6 +14,17 @@ import "../css/Tab1.css";
 //import NutrientDataDisplay from '../../components/NutrientDataDisplay';
 
 const NutritionalDataPage: React.FC = () => {
+  
+  interface PersonalData {
+    uuid: number;
+    height: string | number | null | undefined;
+    weight: string | number | null | undefined;
+    age: string | number | null | undefined;
+    sex: string | number | null | undefined;
+    goal: string | null;
+    food_preferences: Array<string>;
+    dietary_restrictions: Array<string>;
+  }
   interface NutritionalData {
     calories: string | number | null | undefined;
     fat: string | number | null | undefined;
@@ -26,6 +37,8 @@ const NutritionalDataPage: React.FC = () => {
     sodium: string | number | null | undefined;
   }
 
+  
+
   const getLocalData = () => {
     const local = window.localStorage.getItem("data");
     if (local == null) {
@@ -33,6 +46,37 @@ const NutritionalDataPage: React.FC = () => {
     }
     return JSON.parse(local);
   };
+
+  const [personalData, setPersonalData] = useState<PersonalData>(
+    getLocalData() || {
+      uuid: 1234,
+      height: null,
+      weight: null,
+      age: null,
+      sex: null,
+      goal: null,
+      food_preferences: [],
+      dietary_restrictions: [],
+    }
+  );
+
+
+  const collectData = async () => {
+    const data = {
+      uuid: 1234,
+      height: 0,
+      weight: 0,
+      age: 0,
+      sex: 0,
+      goal: 0,
+      food_preferences: 0,
+      dietary_restrictions: 0,
+    };
+  
+  console.log("DATA", data);
+  const url = new URL(`http://127.0.0.1:5000/v1/user/${data.uuid}`);
+  };
+
 
   //https://www.livestrong.com/article/440416-fda-daily-nutritional-requirements/
   const [nutritionalData] = useState({
@@ -63,10 +107,10 @@ const NutritionalDataPage: React.FC = () => {
         <IonHeader>
           <IonItem>Current Reccomended Daily Macronutrient Intake</IonItem>{" "}
         </IonHeader>
-        <IonItem>Daily Caloric Reccomendation: </IonItem>
-        <IonItem>Daily Calcium Reccomendation: </IonItem>
-        <IonItem>Daily Caloric Reccomendation: </IonItem>
-        <IonItem>Daily Caloric Reccomendation: </IonItem>
+        <IonItem>Caloric Reccomendation: </IonItem>
+        <IonItem>Calcium Reccomendation: </IonItem>
+        <IonItem>Caloric Reccomendation: </IonItem>
+        <IonItem>Caloric Reccomendation: </IonItem>
       </IonContent>
     </IonPage>
   );
