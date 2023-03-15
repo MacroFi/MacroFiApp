d5f3c8c0f20a055d0e324f62f1ad82240049abc6
Shoaib Bohra <67881729+EndUserAgreement@users.noreply.github.com>
2023-03-15 05:29:23 -0700

    NutritionPage working

--- src/pages/PersonalDataTab/NutritionalDataPage.tsx
+++ src/pages/PersonalDataTab/NutritionalDataPage.tsx
@@ -13,7 +13,7 @@ import {
 
 const NutritionalDataPage: React.FC = () => {
   interface NutritionalData {
-    calories: string | number | null | undefined;
+    //calories: string | number | null | undefined;
     fat: string | number | null | undefined;
     protein: string | number | null | undefined;
     carbs: string | number | null | undefined;
@@ -33,32 +33,38 @@ const NutritionalDataPage: React.FC = () => {
   };
 
   const [userCal, setUserCal] = useState<any>();
+
   //https://www.livestrong.com/article/440416-fda-daily-nutritional-requirements/
   const [nutritionData, setNutritionData] = useState<NutritionalData>({
-    calories: 0,
-    fat: 0,
-    protein: 0,
-    carbs: 0,
+    fat: 1000,
+    protein: 400,
+    carbs: 600,
   });
 
   const collectData = async () => {
     const url = new URL(`http://127.0.0.1:5000/v1/user/1234/calorie/need`);
     const response = await fetch(url);
     const uData = await response.json();
-    console.log("cals", uData);
-    setUserCal(uData);
+    //console.log("cals", uData.calorie_need);
+    setUserCal(parseInt(uData.calorie_need));
 
     if (response.status === 200) {
       console.log("GOT PERSONAL DATA SUCCESSFULLY");
     } else if (response.status === 400) {
       console.log("FAILED TO UPDATE PERSONAL DATA");
     }
-
-    const url2 = new URL(`http://127.0.0.1:5000/v1/user/1234/macros`);
+    const uuid = 1234
+    const url2 = new URL(`http://127.0.0.1:5000/v1/user/${uuid}/macros`);
     const response2 = await fetch(url2);
     const uData2 = await response2.json();
-    console.log("macros", uData2);
-    setNutritionData(uData2);
+    //console.log("macros", uData2.macronutrients);
+    const temp = uData2.macronutrients // An array of FPC
+    const data = {
+      fat: parseInt(temp[0]),
+      protein: parseInt(temp[1]),
+      carbs: parseInt(temp[2])
+    }
+    setNutritionData(data);
 
     if (response.status === 200) {
       console.log("GOT PERSONAL DATA SUCCESSFULLY");
@@ -86,12 +92,10 @@ const NutritionalDataPage: React.FC = () => {
         <>
           <IonButton onClick={collectData}>Get Daily Recommendations</IonButton>
         </>
-        <IonItem>Caloric Recommended: {userCal}</IonItem>
-        <IonItem>Fat Recommended: </IonItem>
-        <IonItem>Protein Recommended: </IonItem>
-        <IonItem>Cholesterol Recommended: </IonItem>
-        <IonItem>Cholesterol Recommended: </IonItem>
-        <IonItem>Cholesterol Recommended: </IonItem>
+        <IonItem>Caloric Recommended: {userCal} cal</IonItem>
+        <IonItem>Fat Recommended: {nutritionData.fat}g</IonItem>
+        <IonItem>Protein Recommended: {nutritionData.protein}g</IonItem>
+        <IonItem>Carbohydrates Recommended: {nutritionData.carbs}g</IonItem>
       </IonContent>
     </IonPage>
   );
