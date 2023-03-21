aaa1f934f2c4198d2d734ae7244a0887bc999368
Steven Rivera <shrivera@uci.edu>
2023-03-20 21:58:02 -0700

    Adding padding around restaurant cards

--- src/pages/PersonalDataTab/PersonalDataTabMainPage.tsx
+++ src/pages/PersonalDataTab/PersonalDataTabMainPage.tsx
@@ -10,8 +10,13 @@ import {
 } from "@ionic/react";
 
 const PersonalDataMainPage: React.FC = () => {
-  const test = () => {
-    console.log(window.localStorage.getItem("uuid"))
+  const test = async () => {
+    const uuid = window.localStorage.getItem("uuid")
+    const recommendations = new URL(`http://127.0.0.1:5000/v1/user/${uuid}/recommendations`);
+    const meals = new URL(`http://127.0.0.1:5000/v1/user/${uuid}/meals`);
+    const response = await fetch(meals)
+    const json = await response.json()
+    console.log(json)
   }
   return (
     <IonPage>
